cca63343b3f2ea36845e38f37008dca8115bab81
Andre Tauffer <andregiraotauffer@gmail.com>
2020-03-29 22:05:43 +0200

    Fixed import bug

--- client/src/Services/HOC.js
+++ client/src/Services/HOC.js
@@ -1,11 +1,9 @@
 import Composer from "./Composer";
 import withSearchState from "./Search/withSearchState";
 import SearchBoxContainer from "./Search/SearchBoxContainer";
-import withFetch from "./CommonLogic/withFetch";
 
 export default {
   Composer,
   withSearchState,
-  SearchBoxContainer,
-  withFetch
+  SearchBoxContainer
 };
